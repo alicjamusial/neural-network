0a1d67e403d13c46274a8cc3e6ffad004f79e018
alicjamusial <alicja@musial.pro>
2021-12-12 12:15:44 +0100

    Back propagation

--- script.py
+++ script.py
@@ -9,16 +9,21 @@ def sigmoid(value: float) -> float:
     return 1.0 / (1.0 + exp(-value))
 
 
+def sigmoid_derivative(value: float) -> float:
+    return value * (1.0 - value)
+
+
 class Neuron:
     def __init__(self, bias: float, weights: List[float]) -> None:
         self.bias = bias
         self.weights = weights
         self.activation = 0
+        self.delta = 0
 
     def __repr__(self):
         return f'Neuron, activation: {self.activation}, bias: {self.bias}, weights: {self.weights}'
 
-    def set_activation(self, inputs: List[float]) -> None:
+    def compute_and_set_activation(self, inputs: List[float]) -> None:
         # activation = SUM(weight i-1 * input i-1) + bias
 
         activation = self.bias
@@ -26,6 +31,9 @@ class Neuron:
             activation += self.weights[i] * inputs[i]
 
         self.activation = sigmoid(activation)
+
+    def set_error_delta(self, error_delta: float) -> None:
+        self.delta = error_delta
 
 
 class NeuronLayer:
@@ -81,25 +89,50 @@ class NeuronNetwork:
 
         return text
 
-    def forward_propagate(self, data_input: List[float]):
+    def forward_propagate(self, data_input: List[float]) -> List[float]:
         print('Forward propagation...')
         assert len(data_input) == self.input_len
+
         self.input = data_input.copy()
 
         for layer in self.layers:
             new_input = []
             for neuron in layer.neurons:
-                neuron.set_activation(data_input)
+                neuron.compute_and_set_activation(data_input)
                 new_input.append(neuron.activation)
             data_input = new_input
 
         output = data_input  # output is last input, magic
         return output
 
+    def back_propagate(self, expected_output: List[float]):
+        # Calculate an error for each output neuron (last layer)
+        # which will give us input to propagate backwards to previous layers
+        print('Backpropagation...')
+
+        number_of_layers = len(self.layers)
+
+        for index in reversed(range(number_of_layers)):
+            layer = self.layers[index]
+
+            if index == number_of_layers - 1:  # last layer
+                for i, neuron in enumerate(layer.neurons):
+                    error = (neuron.activation - expected_output[i]) * sigmoid_derivative(neuron.activation)
+                    neuron.set_error_delta(error)
+
+            else:  # hidden layers
+                for i, neuron in enumerate(layer.neurons):
+                    next_layer = self.layers[index+1]
+                    error = 0.0
+
+                    for next_neuron in next_layer.neurons:
+                        error += (next_neuron.weights[i] * next_neuron.delta)
+
+                    neuron.set_error_delta(error * sigmoid_derivative(neuron.activation))
 
 def main():
     seed(datetime.datetime.now())
-    network = NeuronNetwork(3, [2, 1, 2])
+    network = NeuronNetwork(3, [2, 1, 2])  # including input layer which is not exactly a layer
     print(network)
 
     output = network.forward_propagate([1, 0])
