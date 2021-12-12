ad891405a5298d9c0e1011174721753a7ecd9772
alicjamusial <alicja@musial.pro>
2021-12-12 13:18:02 +0100

    Prediction

--- script.py
+++ script.py
@@ -155,6 +155,11 @@ class NeuronNetwork:
 
             print(f'Iteration: {iteration}, learning rate: {learning_rate}, error: {sum_error}')
 
+    def predict(self, row: List[float]):
+        output = self.forward_propagate(row)
+        max_probability = max(output)
+        return output.index(max_probability)
+
 
 def main():
     seed(datetime.datetime.now())
@@ -171,12 +176,32 @@ def main():
                [8.675418651, -0.242068655, 1],
                [7.673756466, 3.508563011, 1]]
 
+    xor = [[0, 0, 0],
+           [0, 1, 1],
+           [1, 0, 1],
+           [1, 1, 0]]
+
     outputs = 2
     network = NeuronNetwork(3, [2, 2, outputs])  # including input layer which is not exactly a layer
     print(network)
 
-    network.train(dataset, 0.5, 20, outputs)
+    network.train(dataset, 0.5, 200, outputs)
     print(network)
+
+    dataset_to_predict = [[2.7810836, 2.550537003, 0],
+                          [1.465489372, 2.362125076, 0],
+                          [3.396561688, 4.400293529, 0],
+                          [1.38807019, 1.850220317, 0],
+                          [3.06407232, 3.005305973, 0],
+                          [7.627531214, 2.759262235, 1],
+                          [5.332441248, 2.088626775, 1],
+                          [6.922596716, 1.77106367, 1],
+                          [8.675418651, -0.242068655, 1],
+                          [7.673756466, 3.508563011, 1]]
+
+    for row in dataset_to_predict:
+        prediction = network.predict(row)
+        print('Expected=%d, Got=%d' % (row[-1], prediction))
 
     # network.train()
 
